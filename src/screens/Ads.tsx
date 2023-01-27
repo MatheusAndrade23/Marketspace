115a03dfa30402c6b4dc1f56126a3351715bcac1
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2023-01-26 21:58:22 -0300

    Ads

---
+++ src/screens/Ads.tsx
@@ -0,0 +1,52 @@
+import {
+  ScrollView,
+  Text,
+  VStack,
+  HStack,
+  Heading,
+  Button,
+  Center,
+} from "native-base";
+
+import { Ad } from "@components/Ad";
+
+import { Plus } from "phosphor-react-native";
+
+export const Ads = () => {
+  return (
+    <ScrollView>
+      <VStack flex={1} p={10}>
+        <HStack
+          alignItems="center"
+          mt={5}
+          width="full"
+          position="relative"
+          justifyContent="center"
+        >
+          <Heading color="gray.200" fontSize={22} fontFamily="heading">
+            Meus Anúncios
+          </Heading>
+
+          <Button variant="secondary" position="absolute" right={-14}>
+            <Plus />
+          </Button>
+        </HStack>
+
+        <HStack w="full" justifyContent="space-between" mt={10}>
+          <Text color="gray.300" fontSize={16}>
+            9 anúncios
+          </Text>
+          <Text>Todos</Text>
+        </HStack>
+
+        <Ad
+          title="Luminária pendente"
+          image="https://m.media-amazon.com/images/I/510SeRtQxzL._AC_SX679_.jpg"
+          active={true}
+          used={false}
+          price="45,00"
+        />
+      </VStack>
+    </ScrollView>
+  );
+};
