c54164151ce91f46b9bbf69138b1020191f188db
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2023-01-27 21:53:23 -0300

    Ad

--- src/components/Ad.tsx
+++ src/components/Ad.tsx
@@ -1,4 +1,4 @@
-import { VStack, IStackProps, Image, Text, Heading } from "native-base";
+import { VStack, IStackProps, Image, Text, Heading, Box } from "native-base";
 
 type Props = IStackProps & {
   title: string;
@@ -10,12 +10,53 @@ type Props = IStackProps & {
 
 export const Ad = ({ title, price, used, active, image, ...rest }: Props) => {
   return (
-    <VStack {...rest}>
-      <Image source={image} alt={title} />
-      <Text color="gray.300" fontSize={18} mt={1}>
+    <VStack position="relative" {...rest} w="45%">
+      <Heading
+        textTransform="uppercase"
+        color={active ? "white" : "gray.500"}
+        fontSize={10}
+        bg={!used ? "blue.light" : "gray.200"}
+        position="absolute"
+        zIndex={100}
+        borderRadius={10}
+        py={1}
+        px={2}
+        right={2}
+        top={2}
+      >
+        {used ? "Usado" : "Novo"}
+      </Heading>
+      <Box position="relative">
+        {!active && (
+          <Heading
+            textTransform="uppercase"
+            color="gray.200"
+            fontSize={10}
+            position="absolute"
+            left={2}
+            bottom="2"
+            zIndex={100}
+          >
+            Anúncio Desativado
+          </Heading>
+        )}
+        <Image
+          h={20}
+          w={40}
+          source={{
+            uri: image,
+          }}
+          alt={title}
+          resizeMode="cover"
+          borderRadius={10}
+          blurRadius={active ? 0 : 10}
+        />
+      </Box>
+
+      <Text color={active ? "gray.200" : "gray.400"} fontSize={14} mt={1}>
         {title}
       </Text>
-      <Heading color="gray.100" fontSize={20} mt={1}>
+      <Heading color={active ? "gray.200" : "gray.400"} fontSize={14}>
         R$ {price}
       </Heading>
     </VStack>
