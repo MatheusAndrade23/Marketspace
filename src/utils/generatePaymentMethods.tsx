79099317ad6c270d9d90d64079e3fd5a917301ea
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2023-02-13 16:26:08 -0300

    Generate Payment Methods

---
+++ src/utils/generatePaymentMethods.tsx
@@ -0,0 +1,72 @@
+import { Text, HStack, useTheme } from "native-base";
+
+import {
+  Barcode,
+  QrCode,
+  Bank,
+  Money,
+  CreditCard,
+} from "phosphor-react-native";
+
+type GeneratePaymentMethodsProps = {
+  paymentMethods: string[];
+};
+
+export const GeneratePaymentMethods = ({
+  paymentMethods,
+}: GeneratePaymentMethodsProps) => {
+  const { colors } = useTheme();
+
+  return (
+    <>
+      {paymentMethods.includes("boleto") && (
+        <HStack alignItems="center">
+          <Barcode size={20} color={colors.gray[300]} />
+          <Text ml={2} color="gray.300">
+            Boleto
+          </Text>
+        </HStack>
+      )}
+      {paymentMethods.includes("pix") && (
+        <HStack alignItems="center">
+          <QrCode size={20} color={colors.gray[300]} />
+          <Text ml={2} color="gray.300">
+            Pix
+          </Text>
+        </HStack>
+      )}
+      {paymentMethods.includes("deposit") && (
+        <HStack alignItems="center">
+          <Bank size={20} color={colors.gray[300]} />
+          <Text ml={2} color="gray.300">
+            Depósito Bancário
+          </Text>
+        </HStack>
+      )}
+      {paymentMethods.includes("cash") && (
+        <HStack alignItems="center">
+          <Money size={20} color={colors.gray[300]} />
+          <Text ml={2} color="gray.300">
+            Dinheiro
+          </Text>
+        </HStack>
+      )}
+      {paymentMethods.includes("cash") && (
+        <HStack alignItems="center">
+          <Money size={20} color={colors.gray[300]} />
+          <Text ml={2} color="gray.300">
+            Dinheiro
+          </Text>
+        </HStack>
+      )}
+      {paymentMethods.includes("card") && (
+        <HStack alignItems="center">
+          <CreditCard size={20} color={colors.gray[300]} />
+          <Text ml={2} color="gray.300">
+            Cartão de Crédito
+          </Text>
+        </HStack>
+      )}
+    </>
+  );
+};
