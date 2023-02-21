bbe69ac770f5f01f39d008adc282a3584f00653d
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2023-02-21 19:07:39 -0300

    Show Ad Info

--- src/screens/Ad.tsx
+++ src/screens/Ad.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import {
   ScrollView,
   Text,
@@ -7,12 +8,21 @@ import {
   Image,
   Heading,
   useTheme,
+  useToast,
+  Box,
 } from "native-base";
 
-import { useNavigation } from "@react-navigation/native";
+import { Dimensions } from "react-native";
+
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { AppNavigatorRoutesProps } from "@routes/app.routes";
 
+import Carousel from "react-native-reanimated-carousel";
+
 import { Button } from "@components/Button";
+
+import { AppError } from "@utils/AppError";
+import { api } from "@services/api";
 
 import {
   ArrowLeft,
@@ -24,8 +34,26 @@ import {
   WhatsappLogo,
 } from "phosphor-react-native";
 
+import { ProductDTO } from "../dtos/ProductDTO";
+import { Loading } from "@components/Loading";
+import { GeneratePaymentMethods } from "@utils/generatePaymentMethods";
+
+type RouteParams = {
+  id: string;
+};
+
 export const Ad = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [product, setProduct] = useState({} as ProductDTO);
+
+  const width = Dimensions.get("window").width;
+
   const { colors } = useTheme();
+
+  const route = useRoute();
+  const toast = useToast();
+
+  const { id } = route.params as RouteParams;
 
   const navigation = useNavigation<AppNavigatorRoutesProps>();
 
@@ -33,126 +61,171 @@ export const Ad = () => {
     navigation.navigate("app", { screen: "home" });
   };
 
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const productData = await api.get(`products/${id}`);
+        setProduct(productData.data);
+        setIsLoading(false);
+      } catch (error) {
+        const isAppError = error instanceof AppError;
+        const title = isAppError
+          ? error.message
+          : "Não foi possível receber os dados do anúncio. Tente Novamente!";
+
+        if (isAppError) {
+          toast.show({
+            title,
+            placement: "top",
+            bgColor: "red.500",
+          });
+        }
+      }
+    };
+
+    loadData();
+  }, []);
+
   return (
     <>
-      <ScrollView
-        contentContainerStyle={{ flexGrow: 1 }}
-        showsVerticalScrollIndicator={false}
-      >
-        <VStack flex={1}>
-          <HStack w="full" justifyContent="space-between" mt={10}>
-            <NativeButton variant="secondary" px={5} onPress={handleGoBack}>
-              <ArrowLeft color={colors.gray[200]} />
-            </NativeButton>
-          </HStack>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <ScrollView
+            contentContainerStyle={{ flexGrow: 1 }}
+            showsVerticalScrollIndicator={false}
+          >
+            <VStack flex={1}>
+              <HStack w="full" justifyContent="space-between" mt={10}>
+                <NativeButton variant="secondary" px={5} onPress={handleGoBack}>
+                  <ArrowLeft color={colors.gray[200]} />
+                </NativeButton>
+              </HStack>
 
-          <Image
-            w="full"
-            h={80}
-            mt={2}
-            source={{
-              uri: "https://img.ltwebstatic.com/gspCenter/goodsImage/2022/8/12/3845434535_1017760/6FD9BA9080E2D0D65BCF7BF88D0DCDA9_thumbnail_600x.jpg",
-            }}
-            alt="Ad Image"
-            resizeMode="cover"
-            borderColor="gray.400"
-            borderWidth={1}
-          />
+              <Box
+                position="relative"
+                alignItems="center"
+                justifyContent="center"
+              >
+                {!product.is_active && (
+                  <Heading
+                    flex={1}
+                    textTransform="uppercase"
+                    color="white"
+                    fontSize="lg"
+                    position="absolute"
+                    zIndex={100}
+                    bg="gray.300"
+                    p={1}
+                    w={240}
+                    textAlign="center"
+                    borderRadius={10}
+                  >
+                    Anúncio Desativado
+                  </Heading>
+                )}
+                <Carousel
+                  loop
+                  width={width}
+                  height={320}
+                  autoPlay={product.product_images.length > 1}
+                  data={product.product_images}
+                  scrollAnimationDuration={1000}
+                  renderItem={({ item }) => (
+                    <Image
+                      blurRadius={product.is_active ? 0 : 10}
+                      w="full"
+                      h={80}
+                      source={{
+                        uri: `${api.defaults.baseURL}/images/${item.path}`,
+                      }}
+                      alt="Ad Image"
+                      resizeMode="cover"
+                      borderColor="gray.300"
+                      borderWidth={1}
+                    />
+                  )}
+                />
+              </Box>
 
-          <VStack px={5}>
-            <Heading
-              my={2}
-              textTransform="uppercase"
-              color="blue.default"
-              fontSize={14}
-              mt={4}
-            >
-              Usado
-            </Heading>
-            <HStack w="full" justifyContent="space-between" alignItems="center">
-              <Heading color="gray.200" fontSize={22} fontFamily="heading">
-                Luminária pendente
-              </Heading>
-              <Text color="blue.light" fontFamily="heading">
-                R${" "}
-                <Heading color="blue.light" fontFamily="heading" fontSize={20}>
-                  45,00
+              <VStack px={5}>
+                <Heading
+                  my={2}
+                  textTransform="uppercase"
+                  color="blue.default"
+                  fontSize={14}
+                  mt={4}
+                >
+                  {product.is_new ? "NOVO" : "USADO"}
                 </Heading>
-              </Text>
-            </HStack>
+                <HStack
+                  w="full"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Heading color="gray.200" fontSize={22} fontFamily="heading">
+                    {product.name}
+                  </Heading>
+                  <Text color="blue.light" fontFamily="heading">
+                    R${" "}
+                    <Heading
+                      color="blue.light"
+                      fontFamily="heading"
+                      fontSize={20}
+                    >
+                      45,00
+                    </Heading>
+                  </Text>
+                </HStack>
 
-            <Text mt={2} color="gray.300">
-              Cras congue cursus in tortor sagittis placerat nunc, tellus arcu.
-              Vitae ante leo eget maecenas urna mattis cursus.{" "}
+                <Text mt={2} color="gray.300">
+                  {product.description}
+                </Text>
+
+                <Heading color="gray.300" fontSize={14} my={5}>
+                  Aceita troca?{" "}
+                  <Text fontWeight="normal">
+                    {product.accept_trade ? "Sim" : "Não"}
+                  </Text>
+                </Heading>
+
+                <Heading color="gray.300" fontSize={14} mb={2}>
+                  Meios de Pagamento:
+                </Heading>
+
+                {GeneratePaymentMethods(
+                  product.payment_methods.map(
+                    (payment_method) => payment_method.key
+                  )
+                )}
+              </VStack>
+            </VStack>
+          </ScrollView>
+          <HStack
+            w="full"
+            p={5}
+            bg="white"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Text color="blue.light" fontFamily="heading">
+              R${" "}
+              <Heading color="blue.light" fontFamily="heading" fontSize={24}>
+                {product.price}
+              </Heading>
             </Text>
 
-            <Heading color="gray.300" fontSize={14} my={5}>
-              Aceita troca? <Text fontWeight="normal">Não</Text>
-            </Heading>
-
-            <Heading color="gray.300" fontSize={14} mb={2}>
-              Meios de Pagamento:
-            </Heading>
-
-            <HStack alignItems="center">
-              <Barcode size={20} color={colors.gray[300]} />
-              <Text ml={2} color="gray.300">
-                Boleto
-              </Text>
-            </HStack>
-
-            <HStack alignItems="center">
-              <QrCode size={20} color={colors.gray[300]} />
-              <Text ml={2} color="gray.300">
-                Pix
-              </Text>
-            </HStack>
-
-            <HStack alignItems="center">
-              <Bank size={20} color={colors.gray[300]} />
-              <Text ml={2} color="gray.300">
-                Depósito Bancário
-              </Text>
-            </HStack>
-
-            <HStack alignItems="center">
-              <Money size={20} color={colors.gray[300]} />
-              <Text ml={2} color="gray.300">
-                Dinheiro
-              </Text>
-            </HStack>
-
-            <HStack alignItems="center">
-              <CreditCard size={20} color={colors.gray[300]} />
-              <Text ml={2} color="gray.300">
-                Cartão de Crédito
-              </Text>
-            </HStack>
-          </VStack>
-        </VStack>
-      </ScrollView>
-      <HStack
-        w="full"
-        p={5}
-        bg="white"
-        alignItems="center"
-        justifyContent="space-between"
-      >
-        <Text color="blue.light" fontFamily="heading">
-          R${" "}
-          <Heading color="blue.light" fontFamily="heading" fontSize={24}>
-            45,00
-          </Heading>
-        </Text>
-
-        <Button
-          title="Entrar em contato"
-          icon={<WhatsappLogo color="white" weight="fill" size={20} />}
-          alignItems="center"
-          justifyContent="center"
-          w="50%"
-        />
-      </HStack>
+            <Button
+              title="Entrar em contato"
+              icon={<WhatsappLogo color="white" weight="fill" size={20} />}
+              alignItems="center"
+              justifyContent="center"
+              w="50%"
+            />
+          </HStack>
+        </>
+      )}
     </>
   );
 };
